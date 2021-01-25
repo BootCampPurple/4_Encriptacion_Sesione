ecbe26574bd1085af5df33e9981ad0fbd1c26866
Riccardo Mija <riccardomp@gmail.com>
2021-01-25 15:35:19 -0500

    Version base

--- app/src/main/java/pe/com/bootcamp/encriptacionysesion/ui/MainActivity.kt
+++ app/src/main/java/pe/com/bootcamp/encriptacionysesion/ui/MainActivity.kt
@@ -22,46 +22,6 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        binding.butEncriptar.setOnClickListener {
-            val intent = Intent(this, EncriptarActivity::class.java)
-            startActivity(intent)
-        }
 
-        binding.butIniciarSesion.setOnClickListener {
-            BCPApplication.startSession()
-        }
-
-
-
-
-
-
-        val encService = EncryptionManager(this)
-        encService.createMasterKey()
-
-        //To get public key.
-        val myPublicKey = encService.getMyPublicKey()
-
-
-        //Obtener llave publica para enviarlo a un servidor
-        val myPublicKeyString =
-                String(android.util.Base64.encode(myPublicKey!!.encoded, android.util.Base64.NO_WRAP))
-
-
-        //To get PublicKey from string that shared by others
-        //val othersPublicKey = encService.getOtherPublicKey(myPublicKeyString)
-
-        //Encrypt Message (Using other's public key)
-        val message: String = "Hi, Ross"
-        //val otherEnc = encService.encryptOthers(message, othersPublicKey!!)
-        val otherEnc = encService.encryptOthers(message, myPublicKey)
-
-
-        //Decrypt Message (Which encrypted using my public key)
-        val encryptedString: String
-        val decryptedString = encService.decrypt(otherEnc!!)
-
-
-        val lol = ""
     }
 }
